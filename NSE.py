2afbe50a22e7e7a15e265faee751dce647690d25
lakshay <lakshay21059@iiitd.ac.in>
2026-03-23 10:41:12 +0530

    hardlimit fix

--- NSE.py
+++ NSE.py
@@ -114,7 +114,7 @@ records = data["records"]["data"]
 
 def build_option_row(item, underlying):
     strike = item["strikePrice"]
-    diff = abs(underlying - 22500)
+    diff = max(500, abs(underlying - 22500))
     if abs(strike - underlying) > diff:
         return None
     ce = item.get("CE", {})
